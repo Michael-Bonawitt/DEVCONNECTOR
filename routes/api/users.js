53daa8a410f40b3e0297d9e998454f993a7159ed
Mike Bonawitt <M412bon@yahoo.com>
2021-07-22 09:45:36 -0400

    Initiated JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -48,9 +50,21 @@ async (req, res) => {
 
         await user.save();
 
-        // Return jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
 
-        res.send('User Registered');
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            {expiresIn: 3600},
+            (err, token) =>{
+                if (err) throw err;
+                res.json({token});
+            } 
+        );
 
     } catch(err) {
         console.error(err.message);
