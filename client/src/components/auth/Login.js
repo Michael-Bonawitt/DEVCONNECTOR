515d2373147cc6c05b32a9838223ce535491c5ac
Mike Bonawitt <M412bon@yahoo.com>
2021-10-22 16:15:36 -0400

    Fixed console warnings

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -3,7 +3,6 @@ import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { login } from '../../actions/auth';
-import axios from 'axios';
 
 
 const Login = ({ login, isAuthenticated }) => {
--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from 'react';
 import spinner from './spinner.gif';
 
-export default () => (
+const Spinner = () => (
     <Fragment>
         <img src={spinner} style={{ width: '200px', margin: 'auto', display: 'block' }} alt='Loading...' />
     </Fragment>
 );
+
+export default Spinner;
