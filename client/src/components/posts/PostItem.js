ecc3ae1ef77c7ab9adde6c613536486f42d82b55
Mike Bonawitt <M412bon@yahoo.com>
2021-12-17 16:49:17 -0500

    Single Post Display

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -5,7 +5,9 @@ import Moment from 'react-moment';
 import { connect } from 'react-redux';
 import { addLike, removeLike, deletePost } from '../../actions/post';
 
-const PostItem = ({ addLike, removeLike, deletePost, auth, post: { _id, text, name, avatar, user, likes, comments, date }}) => 
+const PostItem = ({ addLike, removeLike, deletePost, auth, post: { _id, text, name, avatar, user, likes, comments, date },
+showActions
+}) => (
         <div class="post bg-white p-1 my-1">
           <div>
             <Link to={`/profile/${user}`}>
@@ -21,10 +23,11 @@ const PostItem = ({ addLike, removeLike, deletePost, auth, post: { _id, text, na
             <p class="my-1">
               {text}
             </p>
-             <p class="post-date">
+              <p class="post-date">
                 Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
             </p>
-            <button onClick={e => addLike(_id)} type="button" class="btn btn-light">
+            {showActions && <Fragment>
+              <button onClick={e => addLike(_id)} type="button" class="btn btn-light">
               <i class="fas fa-thumbs-up"></i>{' '}
               <span>{likes.length > 0 && (
                 <span>{likes.length}</span>
@@ -33,7 +36,7 @@ const PostItem = ({ addLike, removeLike, deletePost, auth, post: { _id, text, na
             <button onClick={e => removeLike(_id)} type="button" class="btn btn-light">
               <i class="fas fa-thumbs-down"></i>
             </button>
-            <Link to={`/post/${_id}`} class="btn btn-primary">
+            <Link to={`/posts/${_id}`} class="btn btn-primary">
               Discussion {comments.length > 0 && (
                 <span class='comment-count'>{comments.length}</span>
               )} 
@@ -43,10 +46,15 @@ const PostItem = ({ addLike, removeLike, deletePost, auth, post: { _id, text, na
                 <i class="fas fa-times"></i>
               </button>
             )}
+              </Fragment>}
+            
           </div>
         </div>
+);
     
-
+PostItem.defaultProps = {
+  showActions: true
+}
 
 PostItem.propTypes = {
     post: PropTypes.object.isRequired,
